909083d99b5b6debbb56c08de1e9a401ad4b8b76
jiyadhrahman050-cmd <jiyadhrahman050@gmail.com>
2026-07-13 17:26:47 +0530

    change

--- backend/authentication_api/settings.py
+++ backend/authentication_api/settings.py
@@ -64,7 +64,6 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
 
 
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
