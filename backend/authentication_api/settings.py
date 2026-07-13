e3bec2e3207b52abb0e2f1c06c31c16117b8522f
jiyadhrahman050-cmd <jiyadhrahman050@gmail.com>
2026-07-13 16:16:33 +0530

    data json

--- backend/authentication_api/settings.py
+++ backend/authentication_api/settings.py
@@ -98,9 +98,10 @@ WSGI_APPLICATION = 'authentication_api.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=config("DATABASE_URL")
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 
